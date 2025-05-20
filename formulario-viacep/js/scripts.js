f6ecd188cf03597cb28291184389478c033b9ed2
Cristiano <Professor@SN-371163.edu.fiesc.com.br>
2025-05-19 22:20:51 -0300

    feat: criacao do formulario via cep

---
+++ formulario-viacep/js/scripts.js
@@ -0,0 +1,45 @@
+const cepInput = document.querySelector("#cep")
+
+cepInput.addEventListener("blur",()=>{
+    
+    /*
+    Sobre a regex /\D/g
+    /.../ - Delimita a expressão regular
+
+    \D - É o inverso de \d, ou seja, "qualquer caractere que não seja digito" (0 a 9)
+
+    g - É a flag global, que faz a substituição ocorrer em todas as ocorrências do texto.
+
+    Exemplo:
+
+    Se  o usuário digita 88.000-000
+    A função fará '88.000-000'.replace(/\D/g,'') // Resultado: '88000000'
+
+
+    */
+
+    const cep = cepInput.value.replace(/\D/g,'')
+    if(cep.length === 8){
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data =>{
+            if(!data.erro){
+                document.querySelector("#logradouro").value = data.logradouro
+
+                document.querySelector("#complemento").value = data.complemento
+
+                document.querySelector("#bairro").value = data.bairro
+
+                document.querySelector("#cidade").value = data.localidade
+
+                document.querySelector("#estado").value = data.uf
+
+            } else {
+                console.log("Cep não encontrado")
+            }
+        })
+    }else{
+        alert("CEP Inválido")
+    }
+
+})
